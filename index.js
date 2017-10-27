e8978ffcb40fe77eb2b7d3c275880f71833347bf
Joe Abbey <joe.abbey@gmail.com>
2017-10-27 10:30:38 -0400

    refactor'd

--- index.js
+++ index.js
@@ -9,46 +9,36 @@ const contrib = require('blessed-contrib')
 
 const apiURI = 'https://api.gdax.com';
 
+const currencies = ['BTC-USD', 'ETH-USD', 'LTC-USD', 'ETH-BTC', 'LTC-BTC']
 var authenticatedClient = null;
 
 if(config)
 	authenticatedClient = new GDAX.AuthenticatedClient(config.apikey, config.base64secret, config.passphrase, apiURI)
-
-const BTC_USD = 'BTC-USD';
-const ETH_USD = 'ETH-USD';
-const LTC_USD = 'LTC-USD';
-const ETH_BTC = 'ETH-BTC';
-const LTC_BTC = 'LTC-BTC';
-
-const lineChartProduct = BTC_USD;
-
-const USD_websocket = new GDAX.WebsocketClient([BTC_USD, ETH_USD, LTC_USD]);
-const ETH_BTC_websocket = new GDAX.WebsocketClient([ETH_BTC]);
-const LTC_BTC_websocket = new GDAX.WebsocketClient([LTC_BTC]);
-
-//
-// Build a nice grid
-//
+const websocket = new GDAX.WebsocketClient(currencies);
 
 var screen = blessed.screen()
 var grid = new contrib.grid({rows: 8, cols: 5, screen: screen})
 
-var BTC_USD_log  = grid.set(0, 0, 3, 1, contrib.log, {label: "BTC-USD Trade History", tags: true})
-var ETH_USD_log  = grid.set(0, 1, 3, 1, contrib.log, {label: "ETH-USD Trade History", tags: true})
-var LTC_USD_log  = grid.set(0, 2, 3, 1, contrib.log, {label: "LTC-USD Trade History", tags: true})
-var ETH_BTC_log  = grid.set(0, 3, 3, 1, contrib.log, {label: "ETH-BTC Trade History", tags: true})
-var LTC_BTC_log  = grid.set(0, 4, 3, 1, contrib.log, {label: "LTC-BTC Trade History", tags: true})
+var currencyMap = {};
 
-var line = grid.set(3, 0, 5, 5, contrib.line, {label: "BTC-USD Price Chart", minY: 5500.0})
+for (var i = 0, len = currencies.length; i < len; i++) {
 
-var currentPrice = {
-	BTC_USD: 5500.0,
-	ETH_USD: 300.0,
-	LTC_USD: 60.0,
-	ETH_BTC: 0.05,
-	LTC_BTC: 0.01
+	//Instantiate a currency entry
+	currencyMap[currencies[i]] = {
+		currentPrice: 1.0,
+		logger: null
+	}
+
+	//Instantiate the logger
+	currencyMap[currencies[i]].logger =
+		grid.set(0, i, 3, 1, contrib.log, {
+			label: sprintf("%s Trade History", currencies[i]),
+			tags: true
+		})
 }
 
+const lineChartProduct = 'BTC-USD';
+var line = grid.set(3, 0, 5, 5, contrib.line, {label: "BTC-USD Price Chart", minY: 5500.0})
 var tradeChart = {
 	title: 'BTC-USD trade chart',
 	style: {line: 'red'},
@@ -78,64 +68,40 @@ const websocketPriceCallback = (data) => {
 	if ("price" in data) {
 
 		var when = new Date(data.time)
-
-		var logger;
+		var logger = currencyMap[data.product_id].logger;
 		var color_begin = "{red-fg}";
 		var color_end = "{/red-fg}";
 
-		currentPrice[data.product_id] = parseFloat(data.price);
+		currencyMap[data.product_id].currentPrice = parseFloat(data.price);
 
 		if(data.side == "buy") {
 			color_begin = "{green-fg}";
 			color_end = "{/green-fg}";
 		}
 
-		if(data.product_id === 'BTC-USD')
-			logger = BTC_USD_log
-
-		if(data.product_id === 'ETH-USD')
-			logger = ETH_USD_log
-
-		if(data.product_id === 'LTC-USD')
-			logger = LTC_USD_log
-
-		logger.log(sprintf("%s %12.8f    %6.2f    %s %s", color_begin, parseFloat(data.size), parseFloat(data.price), when.toFormat("HH24:MI:SS"), color_end));
+		if(data.product_id === 'ETH-BTC') {
+			var eth_price = currencyMap['ETH-USD'].currentPrice;
+			var btc_price = currencyMap['BTC-USD'].currentPrice;
+			var eth_btc   = currencyMap['ETH-BTC'].currentPrice;
+			logger.log(sprintf("%s%8.6f - %8.6f = %8.6f %s%s", color_begin, eth_btc, eth_price / btc_price, eth_btc - (eth_price / btc_price), when.toFormat("HH24:MI:SS"), color_end));
+		}
+		else if (data.product_id === 'LTC-BTC') {
+			var ltc_price = currencyMap['LTC-USD'].currentPrice;
+			var btc_price = currencyMap['BTC-USD'].currentPrice;
+			var ltc_btc   = currencyMap['LTC-BTC'].currentPrice;
+			logger.log(sprintf("%s%8.6f - %8.6f = %8.6f %s%s", color_begin, ltc_btc, ltc_price / btc_price, ltc_btc - (ltc_price / btc_price), when.toFormat("HH24:MI:SS"), color_end));
+		}
+		else
+			logger.log(sprintf("%s %12.8f    %6.2f    %s %s", color_begin, parseFloat(data.size), parseFloat(data.price), when.toFormat("HH24:MI:SS"), color_end));
 	}
 }
 
-// Stream all of the trades (that are filled)
-const websocketDeltaCallback = (data) => {
-
-	//Just care about what's filled for now
-	if (!(data.type === 'match'))
-		return;
-
-	//Sometimes fills have no price /shrug
-	if ("price" in data) {
-
-		var when = new Date(data.time)
-
-		currentPrice[data.product_id] = parseFloat(data.price);
-
-		if(data.product_id === 'ETH-BTC')
-			ETH_BTC_log.log(sprintf(" %8.6f - %8.6f = %8.6f %s", currentPrice[data.product_id] , currentPrice['ETH-USD'] / currentPrice['BTC-USD'], currentPrice[data.product_id] - (currentPrice['ETH-USD'] / currentPrice['BTC-USD']), when.toFormat("HH24:MI:SS")));
-
-		if(data.product_id === 'LTC-BTC')
-			LTC_BTC_log.log(sprintf(" %8.6f - %8.6f = %8.6f %s", currentPrice[data.product_id] , currentPrice['LTC-USD'] / currentPrice['BTC-USD'], currentPrice[data.product_id] - (currentPrice['LTC-USD'] / currentPrice['BTC-USD']), when.toFormat("HH24:MI:SS")));
-    }
-}
-
-USD_websocket.on('message', websocketPriceCallback);
-
-ETH_BTC_websocket.on('message', websocketDeltaCallback);
-LTC_BTC_websocket.on('message', websocketDeltaCallback);
-
-
+websocket.on('message', websocketPriceCallback);
 
 // Meanwhile let's periodically dispaly the current price in the graph
 function updateChart() {
 	tradeChart.y.shift()
-	tradeChart.y.push(currentPrice['BTC-USD'])
+	tradeChart.y.push(currencyMap['BTC-USD'].currentPrice)
 	line.setData(tradeChart)
 }
 
